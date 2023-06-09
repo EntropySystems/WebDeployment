e8d4696a770aa20e053ad9e4d9f119e5d7f48376
EntropySystems <devlaq4918@gmail.com>
2023-06-09 07:41:59 +0000

    Update from https://github.com/EntropySystems/WebDeployment/commit/0e75ef9e1d88206fb3967f0699b36ffc4e3230d8

---
+++ _app/immutable/chunks/stores.105ecb5c.js
@@ -0,0 +1,1 @@
+import"./index.98b41ded.js";import{s as e}from"./singletons.f5490d4a.js";const r=()=>{const s=e;return{page:{subscribe:s.page.subscribe},navigating:{subscribe:s.navigating.subscribe},updated:s.updated}},b={subscribe(s){return r().page.subscribe(s)}};export{b as p};
